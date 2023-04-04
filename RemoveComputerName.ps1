da9446312ee1b99ff676b47d81ee53824ff55eb8
sanglv97 <76397656+sanglv97@users.noreply.github.com>
2023-04-05 06:14:02 +0700

    Update RemoveComputerName.ps1

--- RemoveComputerName.ps1
+++ RemoveComputerName.ps1
@@ -1,22 +1,23 @@
+clear
 $ErrorActionPreference = 'SilentlyContinue'
 $ComputerNames = Read-Host -Prompt "NHAP COMPUTERNAME MA BAN MUON DELETE"
-$ComputerName = $UserNames.ToLower()
+$ComputerName = $ComputerNames.ToUpper()
 $domain1 = "ominext.local"
-$domain2 = "omicare.ominext.local"
-$UserOminext = Get-ADComputer -Identity $ComputerName -Server $domain1 | Select-Object Name
-$UserOmicare = Get-ADComputer -Identity $ComputerName -Server $domain2 | Select-Object Name                
-if ($UserOminext.Name -eq $ComputerName)                                               
+$domain2 = "omicare.omiext.local"
+$CheckOminext = Get-ADComputer -Identity $ComputerName -Server $domain1 | Select-Object Name
+$CheckOmicare = Get-ADComputer -Identity $ComputerName -Server $domain2 | Select-Object Name
+$GetComputer1 =  $CheckOminext.Name
+$GetComputer2 = $CheckOmicare.Name               
+if ($GetComputer1 -eq $ComputerName)                                               
 {                                                                                 
-Remove-ADComputer -Identity $UserName -Server $domain1                                             
+Remove-ADComputer -Identity $ComputerName -Confirm:$True -Server $domain1                                             
 Write-Host "=> $ComputerName "OMINEXT da duoc xoa!" -ForegroundColor Green                  
 }
-elseif ($UserOmicare.Name -eq $ComputerName) {
-Remove-ADComputer -Identity $ComputerName -Server $domain2                                         
+elseif ($GetComputer2 -eq $ComputerName) {
+Remove-ADComputer -Identity $ComputerName -Confirm:$True -Server $domain2                                     
 Write-Host "=> $ComputerName "OMICARE da duoc xoa" -ForegroundColor Green  
-}                                                                            
-else                                                                            
-{                                                                                 
-Write-Host "=> $ComputerName "khong ton tai!" -ForegroundColor DarkYellow                         
 }
-
+else {
+Write-Host "=> $ComputerName KHONG TON TAI TREN HE THONG"
+}  
 Read-Host -Prompt "[Enter]"
