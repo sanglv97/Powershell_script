55daf9f6558c777d3750a0fa72d686b6b868beee
sanglv97 <76397656+sanglv97@users.noreply.github.com>
2023-04-06 16:38:59 +0700

    Update ResetPassword.ps1

--- ResetPassword.ps1
+++ ResetPassword.ps1
@@ -1,10 +1,33 @@
-# Reset a user's password                                                                             
-$username = Read-Host -Prompt "Enter the username of the account you want to reset the password for"  
-$password = Read-Host -Prompt "Enter the new password for the account" -AsSecureString                
-$user = Get-ADUser -Identity $username -ErrorAction SilentlyContinue                                  
-if ($user -eq $null) {                                                                                
-Write-Host "User not found"                                                                           
-return                                                                                                
-}                                                                                                     
-Set-ADAccountPassword -Identity $username -NewPassword $password -Reset                             
-Write-Host "Password reset successfully"
+# Xác định tên người dùng và mật khẩu mặc định
+$username = Read-Host -Prompt "ten_nguoi_dung"
+$password = Read-Host -Prompt "mat khau" | ConvertTo-SecureString -AsPlainText -Force
+$domain1 = "ominext.local"
+$domain2 = "omicare.ominext.local"
+
+#hiden error when run script
+$ErrorActionPreference = 'SilentlyContinue'
+
+if ($username) {
+    # Thiết lập thuộc tính để yêu cầu người dùng thay đổi mật khẩu lần đầu tiên đăng nhập
+    Set-ADUser -Identity $username -ChangePasswordAtLogon $true $domain1
+    
+    # Nếu người dùng tồn tại, đặt mật khẩu mới cho tài khoản của họ
+    Set-ADAccountPassword -Identity $username -NewPassword $password -Reset -ChangePasswordAtLogon $true
+    
+    # Hiển thị thông báo khi mật khẩu đã được đặt thành công
+    Write-Host "Mật khẩu cho $username (OMINEXT) đã được đặt thành công. Người dùng sẽ được yêu cầu thay đổi mật khẩu lần đầu tiên đăng nhập." -ForegroundColor Green
+}
+elseif ($username) {
+     # Thiết lập thuộc tính để yêu cầu người dùng thay đổi mật khẩu lần đầu tiên đăng nhập
+    Set-ADUser -Identity $username -ChangePasswordAtLogon $true -Server $domain2
+    
+    # Nếu người dùng tồn tại, đặt mật khẩu mới cho tài khoản của họ
+    Set-ADAccountPassword -Identity $username -NewPassword $password -Reset -ChangePasswordAtLogon $true -Server $domain2
+    
+    # Hiển thị thông báo khi mật khẩu đã được đặt thành công
+    Write-Host "Mật khẩu cho $username (OMICARE) đã được đặt thành công. Người dùng sẽ được yêu cầu thay đổi mật khẩu lần đầu tiên đăng nhập." -ForegroundColor Green
+} 
+else {
+    # Nếu người dùng không tồn tại, hiển thị thông báo lỗi
+    Write-Host "Người dùng $username không tồn tại trong hệ thống. Vui lòng kiểm tra lại tên người dùng." -ForegroundColor Red
+}
