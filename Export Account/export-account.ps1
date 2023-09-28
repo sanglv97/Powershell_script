232d6bc658dcbb43adbb6f78f47637edfbcab05b
unknown <administrator@SANG-LV.DE>
2023-09-27 23:04:56 -0700

    new file:   Export Account/export-account.ps1

---
+++ Export Account/export-account.ps1
@@ -0,0 +1,11 @@
+﻿Install-Module -Name ImportExcel -Scope CurrentUser
+Import-Module ActiveDirectory
+
+# Define the path for the Excel file where you want to export the data
+$excelFilePath = "C:\File_account.xlsx"
+
+# Retrieve AD users' usernames and display names
+$users = Get-ADUser -Filter * -Properties DisplayName, SamAccountName | Select-Object SamAccountName, DisplayName
+
+# Export the data to an Excel file
+$users | Export-Excel -Path $excelFilePath -WorksheetName "ADUsers" -AutoSize
